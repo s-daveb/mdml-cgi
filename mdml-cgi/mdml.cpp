3bb3c4bbbe6fb3b4a944f9e8e7d867b248b78420
S David <2100425+s-daveb@users.noreply.github.com>
2023-08-31 19:55:47 -0400

    Set up cmake and dependencies

---
+++ mdml-cgi/mdml.cpp
@@ -0,0 +1,20 @@
+/* ml.cpp
+ * Copyright © 2023 Saul D. Beniquez
+ * License: Mozilla Public License v. 2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v.2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+
+#include "libmdml.hpp"
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
+
+
+// clang-format off
+// vim: set foldmethod=marker foldmarker=#region,#endregion textwidth=80 ts=8 sts=0 sw=8  noexpandtab ft=cpp.doxygen :
---
+++ src/libmdml.cpp
@@ -0,0 +1,18 @@
+/* libmdml.cpp
+ * Copyright © 2023 Saul D. Beniquez
+ * License: Mozilla Public License v. 2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v.2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include <iostream>
+
+void print_hello() {
+	std::cout << "Hello World" << std::endl;
+	return;
+}
+
+// clang-format off
+// vim: set foldmethod=marker foldmarker=#region,#endregion textwidth=80 ts=8 sts=0 sw=8  noexpandtab ft=cpp.doxygen :
