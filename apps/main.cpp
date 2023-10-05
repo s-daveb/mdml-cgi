15f62893f6ac8a1734cc6614ca67f6516e3c328e
S David <2100425+s-daveb@users.noreply.github.com>
2023-10-05 01:57:49 -0400

    Testing the cgi-interface's STDIN and environment variables

--- apps/main.cpp
+++ apps/main.cpp
@@ -7,6 +7,7 @@
  * obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+#include <iomanip>
 #include <iostream>
 #include <map>
 #include <string>
@@ -16,20 +17,49 @@ std::map<std::string, std::string> template_map;
 void
 load_template()
 {
-	template_map["index"] = "<html>\
+	template_map["index"] = L"<html>\
 	<head><title>Hello world</title>\
 	<meta charset=\"UTF-8\"></head>\
 	<body>Testing 1 2 3 ☕️ </body>\
 	</html>";
 }
 
+void
+print_env(char**& envp)
+{
+	std::cout << "Env:" << std::endl;
+
+	auto i = 0;
+	for (auto** env = envp; *env != 0; env++) {
+		auto thisEnv = std::string(*env);
+		++i;
+		std::cout << std::setw(24) << thisEnv << std::setw(0)
+			  << std::flush;
+		if ((i % 8) == 0) {
+			std::cout << std::endl;
+		} else {
+			std::cout << " ";
+		}
+	}
+}
+
 int
-main(int argc, char** argv)
+main(int argc, char** argv, char** envp)
 {
 	load_template();
+	std::cout << "Content-Type: text/plaintext" << std::endl << std::endl;
 
+	print_env(envp);
+	std::cout << std::endl;
+	for (size_t index = 0; index < argc; ++index) {
+		std::cout << argv[index] << " " << std::flush;
+	}
+	std::cout << std::endl;
+
+	/*
 	std::cout << "Content-Type: text/html" << std::endl << std::endl;
 	std::cout << template_map["index"] << std::endl;
+	*/
 	return 0;
 }
 
