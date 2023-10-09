8b4a4ac16afa4d2e555b1650c9d040961c3fab1b
S David <2100425+s-daveb@users.noreply.github.com>
2023-10-09 16:24:57 -0400

    Work #8 - Add demangling support to generate_stacktrace

--- src/debuginfo.cpp
+++ src/debuginfo.cpp
@@ -10,6 +10,7 @@
 #include "debuginfo.hpp"
 
 #ifdef HAVE_EXECINFO_H
+#include <cxxabi.h>
 #include <execinfo.h>
 #else
 #include <boost/stacktrace.hpp>
@@ -19,31 +20,56 @@
 #include <sstream>
 #include <string>
 
-std::string generate_stacktrace(unsigned short framesToRemove) {
-  std::stringstream buffer;
+std::string
+generate_stacktrace(unsigned short framesToRemove)
+{
+	std::stringstream buffer;
+	char* demangled_name;
 
-#ifdef HAVE_EXECINFO_H // @{
-  void *callstack[128];
+	void* callstack[128];
+	int i, frames = backtrace(callstack, 128);
+	char** strs = backtrace_symbols(callstack, frames);
 
-  int i, frames = backtrace(callstack, 128);
-  char **strs = backtrace_symbols(callstack, frames);
-  for (i = framesToRemove; i < frames; ++i) {
-    buffer << strs[i] << std::endl;
-  }
-  free(strs); // @}
-#else
-  buffer << boost::stacktrace::stacktrace() << std::flush;
-#endif
-  return buffer.str();
+	for (i = framesToRemove; i < frames; ++i) {
+		std::stringstream line_stream(strs[i]);
+		std::string word;
+
+		// Split the demangled name by spaces and demangle each word
+		while (line_stream >> word) {
+			if (word.find("0x") == std::string::npos) {
+				int status;
+				char* demangled_word = abi::__cxa_demangle(
+				    word.c_str(), nullptr, nullptr, &status
+				);
+
+				if (status == 0) {
+					buffer << '\t' << demangled_word << ' ';
+					std::free(demangled_word);
+				} else {
+					buffer << word << ' ';
+				}
+			} else {
+				buffer << word << ' ';
+			}
+		}
+
+		buffer << std::endl;
+	}
+
+	free(strs);
+
+	return buffer.str();
 }
 
-void print_cmdline(int argc, const char *argv[]) {
-  int i;
+void
+print_cmdline(int argc, const char* argv[])
+{
+	int i;
 
-  std::cout << "Command-line received" << std::endl;
-  for (i = 0; i < argc; ++i)
-    std::cout << argv[i] << " ";
-  std::cout << std::endl;
+	std::cout << "Command-line received" << std::endl;
+	for (i = 0; i < argc; ++i)
+		std::cout << argv[i] << " ";
+	std::cout << std::endl;
 }
 
 // clang-format off
